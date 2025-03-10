ea22d3cea44c52cd2a2b9566a6d86a7ed5cbc1e2
LeonardoEnnes <leonardoennes@protonmail.com>
2025-03-10 14:20:10 -0300

    feat: implement POST endpoint for creating users

--- src/Controllers/UsersController.cs
+++ src/Controllers/UsersController.cs
@@ -1,11 +1,35 @@
+using AccessTrackAPI.Data;
+using AccessTrackAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccessTrackAPI.Controllers;
 
+// CRUD
 [ApiController]
-[Route("")]
-public class UsersController
+public class UsersController : ControllerBase
 {
-    //[HttpPost("v1/Users/")]
-    // CRUD
+    [HttpPost("v1/Users/")]
+    public async Task<ActionResult> Post(
+        [FromBody] Users user,
+        [FromServices] AccessControlContext context)
+    {
+
+        if (user == null)
+            return BadRequest("User data is required.");
+
+        try
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+            
+            return CreatedAtAction(nameof(Post), new { id = user.Id }, user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine(e.InnerException?.Message);
+            return StatusCode(500, "An error occurred while saving the user.");
+        }
+    }
+
 }
