1b4032e398293c5aad72e9eed5a17d23b21f055f
LeonardoEnnes <leonardoennes@protonmail.com>
2025-03-15 02:43:39 -0300

    feat: add extension methods for ModelState and RoleClaim handling

---
+++ src/Extensions/ModelStateExtention.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AccessTrackAPI.Extensions;
+
+public static class ModelStateExtention
+{
+    public static List<string> GetErrors(this ModelStateDictionary modelState)
+    {
+        var result = new List<string>();
+        foreach (var item in modelState.Values)
+            result.AddRange(item.Errors.Select(error => error.ErrorMessage));
+
+        return result;
+    }
+}
