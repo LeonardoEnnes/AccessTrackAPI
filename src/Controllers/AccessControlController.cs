3e5f7ce6ac3ecef1f606eac96bc8b7bce7918ecf
LeonardoEnnes <leonardoennes@protonmail.com>
2025-03-19 23:32:37 -0300

    feat: add AccessControlController for entry validation and logging

---
+++ src/Controllers/AccessControlController.cs
@@ -0,0 +1,63 @@
+using AccessTrackAPI.Data;
+using AccessTrackAPI.Models;
+using AccessTrackAPI.ViewModels;
+using AccessTrackAPI.ViewModels.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
+
+namespace AccessTrackAPI.Controllers;
+
+[ApiController]
+public class AccessControlController : ControllerBase
+{
+    [HttpPost("v1/Entry/validate-entry")]
+    public async Task<ActionResult> ValidateEntry(
+        [FromBody] LoginViewModel model,
+        [FromServices] AccessControlContext context
+        )
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>("00x00 - Internal Server Error."));
+        
+        var users = await context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == model.Email);
+
+        if (users == null)
+            return BadRequest(new ResultViewModel<string>("username or password is incorrect."));
+        
+        if (!PasswordHasher.Verify(users.PasswordHash, model.Password))
+            return BadRequest(new ResultViewModel<string>("username or password is incorrect."));
+
+        try
+        {
+            // logging the entry
+            var entryLog = new EntryExitLogs
+            {
+                UserId = users.Id,
+                EntryTime = DateTime.UtcNow
+            };
+            
+            // add the entry logs to the database
+            await context.EntryExitLogs.AddAsync(entryLog);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+                {
+                    User = users.Email,
+                    entryTime = entryLog.EntryTime,
+                    message = "Entry logged successfully."
+                }));
+        }
+        catch (DbUpdateException ex)
+        {
+            Console.WriteLine(ex.InnerException?.Message);
+            return StatusCode(400, new ResultViewModel<string>("01x00 - Database Error."));
+        }
+        catch
+        {
+            return StatusCode(400, new ResultViewModel<string>("00x00 - internal server error."));
+        }
+    }
+}
