856053d017b51777c09856e8f9a39844273f7d73
LeonardoEnnes <leonardoennes@protonmail.com>
2025-03-20 22:01:06 -0300

    feat: add DeleteUser endpoint to remove users by ID

--- src/Controllers/AccessControlController.cs
+++ src/Controllers/AccessControlController.cs
@@ -2,6 +2,7 @@ using AccessTrackAPI.Data;
 using AccessTrackAPI.Models;
 using AccessTrackAPI.ViewModels;
 using AccessTrackAPI.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
@@ -58,6 +59,41 @@ public class AccessControlController : ControllerBase
         catch
         {
             return StatusCode(400, new ResultViewModel<string>("00x00 - internal server error."));
+        }
+    }
+    
+    // @desc: remove users from the system
+    [HttpDelete("v1/Admin/DeleteUser/{id}")]
+    [Authorize(Roles = "admin")] // Requires a valid token and the "admin" role
+    public async Task<IActionResult> DeleteUser(
+        [FromRoute] int id,
+        [FromServices] AccessControlContext context)
+    {
+        try
+        {
+            // Searching the user by id
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null)
+                return BadRequest(new ResultViewModel<string>("User not found."));
+            
+            // Removing the user from the system
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+            
+            return Ok(new ResultViewModel<string>($"User {id} deleted successfully."));
+        }
+        catch (DbUpdateException ex)
+        {
+            Console.WriteLine(ex.InnerException?.Message);
+            return StatusCode(400, new ResultViewModel<string>("00x10 - Database Update Error."));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("00x01 - Internal server error."));
         }
     }
 }
