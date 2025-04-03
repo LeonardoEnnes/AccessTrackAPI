39fc8006f1b30938265629312cab1d06cd1a2ffd
LeonardoEnnes <leonardoennes@protonmail.com>
2025-04-03 15:25:31 -0300

    chore: add Swagger for API documentation

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using AccessTrackAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 LoadConfiguration(builder);
@@ -12,26 +13,74 @@ ConfigureAuthentication(builder);
 ConfigureServices(builder);
 ConfigureMvc(builder);
 
-// Add services for controllers
-builder.Services.AddOpenApi(); // Swagger (later)
+// Add Swagger services
+ConfigureSwagger(builder);
 
 // register IHttpContextAccessor
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-app.MapControllers();
-// app.UseHttpsRedirection();
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     builder.Configuration.AddUserSecrets<Program>();
     Console.WriteLine("Development Environment");
-    // app.MapOpenApi();
+    
+    // Enable Swagger UI in development
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AccessTrackAPI V1");
+    });
 }
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 Console.Clear();
 app.Run();
+
+void ConfigureSwagger(WebApplicationBuilder builder)
+{
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "AccessTrackAPI", Version = "v1" });
+        
+        // Add JWT Authentication support to Swagger
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+        
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    });
+}
 
 void ConfigureMvc(WebApplicationBuilder builder)
 {
@@ -39,7 +88,7 @@ void ConfigureMvc(WebApplicationBuilder builder)
     builder.Services.AddControllers();
 }
 
-void LoadConfiguration(WebApplicationBuilder  builder)
+void LoadConfiguration(WebApplicationBuilder builder)
 {
     // Load the JWT key from configuration
     Configuration.JwtKey = builder.Configuration["JwtKey"];
