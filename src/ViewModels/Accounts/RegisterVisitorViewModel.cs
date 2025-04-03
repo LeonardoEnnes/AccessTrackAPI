5bade057d274d0ca5e162a32579ba77b8ec61ab0
LeonardoEnnes <leonardoennes@protonmail.com>
2025-04-03 13:08:29 -0300

    feat: Add RegisterVisitorViewModel to improve VisitorController responses

---
+++ src/ViewModels/Accounts/RegisterVisitorViewModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AccessTrackAPI.ViewModels.Accounts;
+
+public class RegisterVisitorViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is invalid")] 
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", 
+        ErrorMessage = "Password must contain at least one uppercase, one lowercase, one number, and one special character")]
+    public string Password { get; set; }
+    
+    [Required(ErrorMessage = "Telephone number is required")]
+    [Phone(ErrorMessage = "Telephone number is invalid")]
+    [RegularExpression(@"^(\+?\d{1,3}[- ]?)?\d{10}$", 
+        ErrorMessage = "Phone number must be 10 digits")]
+    public string TelephoneNumber { get; set; }
+    
+    [Required(ErrorMessage = "Purpose of visit is required")]
+    [StringLength(500, ErrorMessage = "Purpose cannot exceed 500 characters")]
+    public string Purpose { get; set; }
+}
